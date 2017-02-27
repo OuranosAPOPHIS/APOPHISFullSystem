b51f438adc4d1a1eb3d5d04bcb5b23cd6eb0658e
Dmitry Ivanov <dima.live.here@gmail.com>
2017-02-27 20:40:58 +0300

    Remove Debug folder from git.
    Debug folder and it content is generated during build process every
    time. Every snapshot must be compilable without content of the Debug folder.
    We will be use .gitignore to not show this folder as untracked.

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Tivaware/TM4C1294XL/driverlib/ccs/Debug/driverlib.lib" -l"C:/Tivaware/TM4C1294XL/sensorlib/ccs/Debug/sensorlib.lib"
-
